41d6a93c4bf8186ddd97017b024459167deca2f6
silwal25 <lalitsilwal.2599@gmail.com>
2022-05-09 18:39:48 +0530

    added wrap effect on navigation

---
+++ components/Context.jsx
@@ -0,0 +1,26 @@
+import { useReducer, useContext, createContext, useState } from "react"
+
+const StateContext = createContext()
+const DispatchContext = createContext()
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "":
+      break
+    default:
+      throw new error("Unknown action: " + action.type)
+  }
+}
+
+export const StateProvider = ({ children }) => {
+  const [globalState, setState] = useState({})
+  const [state, dispatch] = useReducer(reducer, globalState)
+  return (
+    <DispatchContext.Provider value={dispatch}>
+      <StateContext.Provider value={state}>{children}</StateContext.Provider>
+    </DispatchContext.Provider>
+  )
+}
+
+export const useState = () => useContext(StateContext)
+export const useDispatch = () => useContext(DispatchContext)
